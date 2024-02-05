f44b5157cef145837bc3ce58a77de608cdd9f171
Utkarsh <104209135+utkarshpayal@users.noreply.github.com>
2024-02-06 03:14:16 +0530

    Update index.js

--- holesale-cli/index.js
+++ holesale-cli/index.js
@@ -1,64 +1,74 @@
-const { spawn } = require('child_process');
+//require modules
+const { exec } = require('child_process');
+const fs = require('fs');
+const Tail = require('tail').Tail;
+const util = require('util');
+const execPromise = util.promisify(exec);
 
+//get user arguments
 const args = process.argv.slice(2);
 
 if (args.length < 1) {
   console.error("No arguments provided. Try holesale-cli --help for a list of all arguments");
   process.exit(1);
 }
-
-const inputs = [];
-
-for (let i = 0; i < args.length; i++) {
-  const [arg, value] = args[i].split("=");
-  const input = {
-    arg,
-    value
-  };
-  inputs.push(input);
-}
-
+//clean user arguments
+const inputs = args.map(arg => {
+  const [key, value] = arg.split("=");
+  return { key, value };
+});
+//start building command for seed generation two
 let hyperServe = "hypertele-server ";
-let seed = "1933949853b7a323ab8f8364e8035c28933554985504a3230afa4d0b4f2aaf01";
-
-for (const { arg, value } of inputs) {
-  if (arg === "--help") {
+let seed = "";
+let seed2 = "";
+//loop through all the parameters
+for (const { key, value } of inputs) {
+  if (key === "--help") {
     console.log("Example: holesale-cli --expose=LOCAL_PORT --server=SERVER_PORT");
     console.log("Where LOCAL_PORT is the port you want to expose on your system, and SERVER_PORT is the port that it will be exposed to on the server.");
   }
 
-  if (arg === "--expose") {
-    hyperServe += "-l " + value + " ";
+  if (key === "--expose") {
+    hyperServe += `-l ${value} `;
   }
 }
 
-// Append the seed
-hyperServe += "--seed " + "'" + seed + "'";
+// Generate seed and store in a file, then read the seed from the file
+exec("hyper-cmd-util-keygen --gen_seed > seed.txt", (error, stdout, stderr) => {
 
-// Use spawn for interactive shell
-// const childProcess = spawn('sh', ['-c', hyperServe], { stdio: 'inherit' });
+  // Read the seed from the file and delete it
+  let filename = "seed.txt";
+  seed = fs.readFileSync(process.cwd() + "/" + filename).toString().substring(6).trim();
+  fs.unlinkSync(filename);
+  // Append the seed
+  hyperServe += `--seed '${seed}'`;
 
+  console.log(seed);
 
-// childProcess.on('exit', (code, signal) => {
-//   if (code !== null) {
-//     console.log(`Process exit code: ${code}`);
-//   } else if (signal !== null) {
-//     console.log(`Process received signal: ${signal}`);
-//   }
-// });
+// Generate seed and store in a file, then read the seed from the file
+exec(hyperServe + " > seed2.txt", (error2, stdout2, stderr2) => {
+  //this will never execute as the above command will run indefinately
+  console.log("tessst")
 
-function run(hyperServe, cb) {
-  var spawn = require('child_process').spawn;
-  var command = spawn(hyperServe);
-  var result = '';
-  command.stdout.on('data', function(data) {
-    result += data.toString();
-  });
-  command.on('close', function(code) {
-    cb(result);
-  });
-}
-
-run(hyperServe, function(message) {
-  console.log(message);
 });
+
+  //read from the file
+  tail = new Tail("seed2.txt");
+  tail.on("line", function(data) {
+    seed2 += data.toString().substring(11).trim();
+    //console.log(seed2)
+    if(seed2.length > 2){
+      tail.unwatch();
+      console.log(seed2);
+      fs.unlinkSync("seed2.txt");
+    }
+  });
+
+
+  return;
+});
+
+
+
+
+
